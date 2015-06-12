d1498e6b7f894a7659e6c65340996b49dd82df2f
daltonpurnell <daltonpurnell@live.com>
2015-06-12 15:36:06 -0600

    adds model objects

---
+++ AnxietyTracker/AnxietyTracker/Entry.h
@@ -0,0 +1,13 @@
+//
+//  Entry.h
+//  AnxietyTracker
+//
+//  Created by Dalton on 6/12/15.
+//  Copyright (c) 2015 Dalton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Entry : NSObject
+
+@end
---
+++ AnxietyTracker/AnxietyTracker/Entry.m
@@ -0,0 +1,13 @@
+//
+//  Entry.m
+//  AnxietyTracker
+//
+//  Created by Dalton on 6/12/15.
+//  Copyright (c) 2015 Dalton. All rights reserved.
+//
+
+#import "Entry.h"
+
+@implementation Entry
+
+@end
