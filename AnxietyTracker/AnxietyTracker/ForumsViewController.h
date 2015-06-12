4a284a6244fbdba7278ef1e2602e9037c2d4662b
daltonpurnell <daltonpurnell@live.com>
2015-06-12 15:19:21 -0600

    adds class files

---
+++ AnxietyTracker/AnxietyTracker/ForumsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForumsViewController.h
+//  AnxietyTracker
+//
+//  Created by Dalton on 6/12/15.
+//  Copyright (c) 2015 Dalton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForumsViewController : UIViewController
+
+@end
---
+++ AnxietyTracker/AnxietyTracker/GetHelpViewController.h
@@ -0,0 +1,13 @@
+//
+//  GetHelpViewController.h
+//  AnxietyTracker
+//
+//  Created by Dalton on 6/12/15.
+//  Copyright (c) 2015 Dalton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GetHelpViewController : UIViewController
+
+@end
